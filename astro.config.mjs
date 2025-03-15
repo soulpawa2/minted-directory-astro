5ac15327c926c726a1a366f7b43a5e51aaf10452
Mark Bruderer <markbruderer123@gmail.com>
2025-03-15 13:42:11 +0100

    feat: upgrade to tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
-import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import icon from 'astro-icon';
 import sitemap from '@astrojs/sitemap';
 import { ViteToml } from 'vite-plugin-toml';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://bestmeditationapps.com",
   integrations: [
     vue(),
-    tailwind(),
     mdx(),
     icon(),
     sitemap()
   ],
   vite: {
-    plugins: [ViteToml()]
+    plugins: [tailwindcss(), ViteToml()]
   }
 });
